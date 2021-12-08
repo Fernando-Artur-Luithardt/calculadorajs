90a3e8f61c70aa85895b2965a257191a34768b5c
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2021-12-08 15:13:01 -0300

    att lógica render e função mais
    ainda falta corrigir para que n1 mude de valor

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 let n = 0
+let n1 = 0
+let n2 = 0
 render(0)
 
 function render(n) {
+
+
+    if (n1 != 0) {
+        tela.innerHTML = ""
+    }
+
     const tela = document.querySelector("#tela")
-    console.log(tela.innerText)
     if (tela.innerHTML != 0) {
         let hist = tela.innerHTML
         tela.innerHTML = hist + n
     }else(tela.innerHTML = n)
+}
+
+function mais() {
+    const tela = document.querySelector("#tela")
+    console.log("mais")
+    if(n1 = 0) {
+        n1 = tela.innerHTML
+    } else if(n1 != 0){
+        n2 = tela.innerHTML
+    } else if (n1 != 0 && n2 != 0) {
+        n1 = n1 + n2
+        render(n1)
+        n1 = 0
+        n2 = 0
+    }
 }
