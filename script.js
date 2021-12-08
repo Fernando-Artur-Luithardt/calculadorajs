cae318dd297e74d29932c8102be76d9e21f89885
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2021-12-08 16:56:18 -0300

    function menos e resultado atualizados funcionando
    criado let menoss para validar com a função resultado, função menos = função somar somente alterado alguns parametros como let maiss para menoss

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let chamou = 0
 //se a função foi chamada
 // 0 falso e 1 verdadeiro
 let maiss = 0
+let menoss = 0
 
 render(telaNum)
 
@@ -31,7 +32,7 @@ function render(n) {
 }
 
 function mais() {
-    console.log(n3 + 'n3')
+    //console.log(n3 + 'n3')
     maiss = 1
     const tela = document.querySelector("#tela")
     if (n1 == 0) {
@@ -52,9 +53,42 @@ function mais() {
     }
 }
 
+function menos() {
+    //console.log(n3 + 'n3')
+    menoss = 1
+    const tela = document.querySelector("#tela")
+    if (n1 == 0) {
+        n1 = tela.innerHTML
+        telaNum = 0   
+    } else if(n1 != 0 && n2 == 0){
+        n2 = tela.innerHTML
+        n1 = parseFloat(n1)
+        n2 = parseFloat(n2)
+        n3 = n1 - n2
+        telaNum = n3
+        tela.innerHTML = ""
+        render(n3)
+        n1 = 0
+        n2 = 0
+        chamou = 1
+        menoss = 0
+    }
+}
+
+function limpa() {
+    n1 = 0
+    n2 = 0
+    n3 = 0
+    chamou = 1
+    render(0)
+}
+
 function resultado(){
     if(maiss == 1) {
         mais()
         maiss = 0
+    }else if (menoss == 1) {
+        menos()
+        menoss = 0
     }
 }
