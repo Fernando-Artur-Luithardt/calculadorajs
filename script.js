0f8b149adef0e0ff3df01cd97237f6fb808e614d
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2021-12-08 15:26:48 -0300

    função mais funcionando
    ainda precisa corrigir o render, fazer zerar a tela apenas uma vez

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function render(n) {
     const tela = document.querySelector("#tela")
 
     if (n1 != 0) {
-        tela.innerHTML = ""
+        tela.innerHTML = 0
     }
 
     if (tela.innerHTML != 0) {
@@ -18,12 +18,22 @@ function render(n) {
 }
 
 function mais() {
+    console.log(n1 + 'n1')
+    console.log(n2 + 'n2')
     const tela = document.querySelector("#tela")
     if(n1 == 0) {
         n1 = tela.innerHTML
-    } else if(n1 != 0){
+    } else if(n1 != 0 && n2 == 0){
         n2 = tela.innerHTML
+        n1 = parseFloat(n1)
+        n2 = parseFloat(n2)
+        n1 = n1 + n2
+        render(n1)
+        n1 = 0
+        n2 = 0
     } else if (n1 != 0 && n2 != 0) {
+        n1 = parseFloat(n1)
+        n2 = parseFloat(n2)
         n1 = n1 + n2
         render(n1)
         n1 = 0
