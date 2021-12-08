bf36141a9ead3b3d3378bcd13e26e1e35bd562cb
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2021-12-08 15:35:58 -0300

    nova lógica para botão =
    var boleana para saber qual das ulimas funções que foi chamada

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let n = 0
 let n1 = 0
 let n2 = 0
-render(0)
+let telaNum = 0
+
+//se a função foi chamada
+// 0 falso e 1 verdadeiro
+let maiss = 0
+
+render(telaNum)
 
 function render(n) {
 
@@ -18,20 +24,12 @@ function render(n) {
 }
 
 function mais() {
-    console.log(n1 + 'n1')
-    console.log(n2 + 'n2')
+    maiss = 1
     const tela = document.querySelector("#tela")
     if(n1 == 0) {
-        n1 = tela.innerHTML
+        n1 = tela.innerHTML   
     } else if(n1 != 0 && n2 == 0){
         n2 = tela.innerHTML
-        n1 = parseFloat(n1)
-        n2 = parseFloat(n2)
-        n1 = n1 + n2
-        render(n1)
-        n1 = 0
-        n2 = 0
-    } else if (n1 != 0 && n2 != 0) {
         n1 = parseFloat(n1)
         n2 = parseFloat(n2)
         n1 = n1 + n2
